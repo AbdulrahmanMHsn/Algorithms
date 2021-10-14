848c0e0c07f19f59232e33416e3e34437a2f45ca
abdelrahman <a.mostafahsn@gmail.com>
2021-10-14 02:16:48 +0200

    Initial commit

---
+++ src/search/BinarySearch.kt
@@ -0,0 +1,43 @@
+package search
+
+/**
+ * Binary search is an algorithm which finds the position of a target value within an array (Sorted)
+ *
+ * * Worst-case performance	O(log(n))
+ * * Best-case performance	O(1)
+ * * Average performance	O(log(n))
+ *
+ * @param list is an array where the element should be found
+ * @param item is the element which should be found
+ * @return index of element if found or -1 if not found
+ */
+
+fun <T : Comparable<T>> binarySearch(list:List<T>,item:T):Int{
+
+    var start = 0
+
+    var end = list.size-1
+
+    var mid:Int
+
+    if (item == list[start]){
+        return start
+    }
+
+    if (item == list[end]){
+        return end
+    }
+
+    while(start <= end){
+
+        mid = (start+end)/2
+
+        when{
+            item > list[mid] -> start = mid+1
+            item < list[mid] -> end = mid-1
+            else -> return mid
+        }
+    }
+
+    return -1
+}
